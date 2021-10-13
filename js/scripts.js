3b83e23bdcd8130decaf2250c420e3247e082eb3
Enadabuzaid <enad.abuzaid15@gmail.com>
2021-10-13 11:34:50 +0300

    add home section

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,27 @@ window.onload = () =>{
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
+
+// custome swiper js 
+var swiper = new Swiper(".book-slider", {
+    loop:true,
+    centerdSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1024: {
+        slidesPerView: 3,
+       
+      },
+    },
+  });
