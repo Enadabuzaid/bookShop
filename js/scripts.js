3c0f387a80f4932b8d01d0b9a26446a6d5fc80c5
Enadabuzaid <enad.abuzaid15@gmail.com>
2021-10-14 10:47:12 +0300

    add new arrivles section

--- js/scripts.js
+++ js/scripts.js
@@ -92,3 +92,28 @@ var swiper = new Swiper(".featured-slider", {
       },
     },
   });
+
+  // custum slide for new arrivals
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween:10,
+    loop:true,
+    centerdSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1024: {
+        slidesPerView: 3,
+       
+      },
+    },
+  });
